bdb7e522f885390463414ceb7ec81710b62e5503
Richard Rafael Sacramento Soares <000001510800@santacasasjc.com.br>
2024-05-29 12:04:06 -0300

    inicio

---
+++ cadastro_reserva.php
@@ -0,0 +1,42 @@
+<form action="cadastrar.php">
+  <span>
+    Nome
+    <input type="text" id="nome">
+  </span>
+
+  <span>
+    Foto
+    <input type="file">
+  </span>
+
+  <span>
+    Data de uso
+    <input type="date">
+  </span>
+
+  <span>
+    Hora Início do uso
+    <input type="time">
+  </span>
+
+  <span>
+    Hora Final do uso
+    <input type="time">
+  </span>
+
+  <span>
+    Resposavel pelo uso
+    <input type="text">
+  </span>
+
+  <span>
+    informações em geral
+    <input type="text">
+  </span>
+
+  <span>
+    Convidados
+    <input type="text">
+  </span>
+  <button></button>
+</form>
---
+++ index.php
@@ -0,0 +1,11 @@
+<form action="valida_login.php">
+  <p>
+    Usuario<input type="text" name="user">
+  </p>
+
+
+  <p>
+    Senha <input type="password" name="password">
+  </p>
+  <button>Logar</button>
+</form>
