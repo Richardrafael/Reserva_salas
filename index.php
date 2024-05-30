8fe221da1c4f48bac6e2780209e354ba7216376d
richardrafael <richard.rafael.soares@gmail.com>
2024-05-30 19:16:08 -0300

    a

--- index.php
+++ index.php
@@ -1,45 +1,43 @@
 <?php
 session_start();
-include 'conexa.php';
+// include 'conexa.php';
 
-// echo md5('senha_secreta');
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//   $username = $_POST['username'];
+//   $password = $_POST['password'];
+//   $senha = md5($password);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  $senha = md5($password);
+//   $sql = "SELECT * FROM usuarios WHERE username = ?  and  senha = ?";
+//   $vert = mysqli_prepare($conn, $sql);
+//   mysqli_stmt_bind_param($vert, 'ss', $username, $senha);
+//   if ($vert) {
+//     mysqli_stmt_execute($vert);
+//     mysqli_stmt_store_result($vert);
+//     if (!mysqli_stmt_num_rows($vert)) {
+//       echo 'usuario nao encontrado';
+//     } else {
+//       $_SESSION['loggedin'] = true;
+//       header('Location: home.php');
+//       exit();
+//     }
+//   }
+// $result = $conn->query($sql);
 
-  $sql = "SELECT * FROM usuarios WHERE username = ?  and  senha = ?";
-  $vert = mysqli_prepare($conn, $sql);
-  mysqli_stmt_bind_param($vert, 'ss', $username, $senha);
-  if ($vert) {
-    mysqli_stmt_execute($vert);
-    mysqli_stmt_store_result($vert);
-    if (!mysqli_stmt_num_rows($vert)) {
-      echo 'usuario nao encontrado';
-    } else {
-      $_SESSION['loggedin'] = true;
-      header('Location: home.php');
-      exit();
-    }
-  }
-  // $result = $conn->query($sql);
+// if ($result->num_rows > 0) {
+//   $row = $result->fetch_assoc();
+//   if (password_verify($password, $row['password'])) {
+//     $error = "Senha incorreta.";
+//   } else {
+//     $_SESSION['loggedin'] = true;
+//     header('Location: home.php');
+//     exit();
+//   }
 
-  // if ($result->num_rows > 0) {
-  //   $row = $result->fetch_assoc();
-  //   if (password_verify($password, $row['password'])) {
-  //     $error = "Senha incorreta.";
-  //   } else {
-  //     $_SESSION['loggedin'] = true;
-  //     header('Location: home.php');
-  //     exit();
-  //   }
-
-  //   echo $password;
-  // } else {
-  //   $error = "Usuário não encontrado.";
-  // }
-}
+//   echo $password;
+// } else {
+//   $error = "Usuário não encontrado.";
+// }
+// }
 ?>
 
 <!DOCTYPE html>
@@ -54,7 +52,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <input type="text" name="username" placeholder="Usuário" required>
     <input type="password" name="password" placeholder="Senha" required>
     <button type="submit">Login</button>
-    <?php if (isset($error)) echo "<p>$error</p>"; ?>
+    <h1>aaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+    <!-- <?php if (isset($error)) echo "<p>$error</p>"; ?> -->
   </form>
 </body>
 
---
+++ usuarios.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 30/05/2024 às 22:55
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `reserva_salas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `senha`) VALUES
+(1, 'usuario_exemplo', '*3D985AFD3A57C0D1B42B672006294D1BB16F3750'),
+(2, '000001510800', '*A4B6157319038724E3560894F7F932C8886EBFCF'),
+(3, 'a', '*E6CC90B878B948C35E92B003C792C46C58C4AF40'),
+(4, 'richard', '81dc9bdb52d04dc20036dbd8313ed055'),
+(5, 'f', 'c4ca4238a0b923820dcc509a6f75849b');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
